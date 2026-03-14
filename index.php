9c0dc1553998428a86c6c24470136ddb6d709cf1
Sala D1 <CSalaD1@uniboyaca.edu.co>
2026-03-14 10:53:23 -0500

    Php con html

--- index.php
+++ index.php
@@ -1,124 +1,23 @@
 <?php
-echo "Hola mundo!!!";
-$nombre = "Pepe";
-echo "\n" . $nombre;
-echo "\n$nombre";
-echo '\n$nombre';
-$apellido = 'Gomez'; //string
-/**
- * BLoque
- * 
- */
-$edad = 18;
-$promedio = 12.56;
-$estado = true; //false 
-$numeros = [1, 2, 3, 4, 5, 6];
-$numeros = array(1, 2, 3, 4, 5, 6);
-// $numeros = new Numeros();
-$prueba = null;
-
-function saludar($nombre){
-    echo "\n Hola $nombre";
+$msg = "Hola mundo!!!";
+function sumar($val1, $val2){
+    return $val1 + $val2;
 }
-saludar("Ana");
-
-function saludar2($nombre){
-    return "\nHola $nombre";
-}
-echo saludar2('Pepe');
-
-//--------------------------------
-$categoria = 1;
-
-if($categoria==1){
-    //.....
-}else if($categoria==2){
-    //....
-}elseif($categoria==3){
-    //....
-}else{
-    //...
-}
-
-/**
- * && and
- * || or
- * ! negacion
- * == igual
- * != diferente
- * < menor que
- * <= menor o igual que
- * > mayor que
- * >= mayor o igual que
- */
-switch($categoria){
-    case 1:
-        //.....
-        break;
-    case 2:
-        //.....
-        break;
-    case 3:
-        //.....
-        break;
-    default:
-        //.....
-        break;
-}
-
-echo "\n\n";
-
-//ciclos-------------------------
-
-
-$numeros = [1,2,3,4,5,6,7,8,9,10];
-// For
-for($index=0; $index<count($numeros);$index++){
-    $numero = $numeros[$index];
-    if(($numero%2)==0){
-        echo "\n $numero es par";
-    }else{
-        echo "\n $numero es impar";
-    }
-}
-echo "\n\n";
-$index = 0;
-while($index<count($numeros)){
-    $numero = $numeros[$index];
-    if(($numero%2)==0){
-        echo "\n $numero es par";
-    }else{
-        echo "\n $numero es impar";
-    }
-    $index++;
-}
-
-echo "\n\n";
-$index = 0;
-do {
-    $numero = $numeros[$index];
-    if(($numero%2)==0){
-        echo "\n $numero es par";
-    }else{
-        echo "\n $numero es impar";
-    }
-    $index++;
-} while ($index<count($numeros));
-
-echo "\n\n";
-foreach($numeros as $valor){
-    if(($valor%2)==0){
-        echo "\n $valor es par";
-    }else{
-        echo "\n $valor es impar";
-    }
-}
-
-echo "\n\n";
-foreach($numeros as $index=>$valor){
-    if(($valor%2)==0){
-        echo "\n $index: $valor es par";
-    }else{
-        echo "\n $index: $valor es impar";
-    }
-}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Inicio php</title>
+</head>
+<body>
+    <h1><?php echo $msg; ?></h1>
+    <p>Resultado de la suma <?php echo sumar(10, 15); ?></p>
+    <br>
+    <form action="validar_numero.php" method="post">
+        <label>Ingrese un número: </label>
+        <input type="number" min="1" name="numero" required>
+        <button type="submit">Validar</button>
+    </form>
+</body>
+</html>
---
+++ validar_numero.php
@@ -0,0 +1,28 @@
+<?php
+// $numero = $_GET['numero'];
+if(empty($_POST['numero'])){
+    header("Location: index.php");
+}
+$numero = $_POST['numero'];
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Validar Número</title>
+</head>
+
+<body>
+    <h1>Validar Número</h1>
+    <?php
+    if (($numero % 2) == 0) {
+        echo "<p>El número $numero es par</p>";
+    } else {
+        echo "<p>El número $numero es impar</p>";
+    }
+    ?>
+    <a href="index.php">Volver</a>
+</body>
+
+</html>
