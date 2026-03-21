2c4b862e5f006ea024e46d456a3e0660c1ced85e
Sala D1 <CSalaD1@uniboyaca.edu.co>
2026-03-21 12:19:07 -0500

    Monolitico 2

--- monolitico/views/lista_estudiantes.php
+++ monolitico/views/lista_estudiantes.php
@@ -17,6 +17,7 @@ $lista = $controller->getListaEstudiantes();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estudiantes</title>
+    <link rel="stylesheet" href="../public/css/index.css">
 </head>
 
 <body>
