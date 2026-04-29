7a44889dff53bfef2d4e5a0db6f9748de2a87dac
Sala D1 <sala_d1@uniboyaca.edu.co>
2026-04-29 18:42:01 -0500

    Introducción a js

--- contactos_ms/public/index.php
+++ contactos_ms/public/index.php
@@ -5,12 +5,9 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ .'/../app/Config/database.php';
 
 $endpoints = require __DIR__ . '/../app/contactos/Presentation/Routers/endpoints.php';
-$endpointsAmigos = require __DIR__ . '/../app/Amigos/Presentation/Routers/endpoints.php';
-
 
 $app = AppFactory::create();
 
 $endpoints($app);
-$endpointsAmigos($app);
 
 $app->run();
